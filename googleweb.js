356243279ee0cb020a53c2921a1aea4296bbfc88
satyakamtyagi369 <satyakamtyagi8979@gmail.com>
2024-01-25 11:04:02 +0530

    add event listner on navbox

--- googleweb.js
+++ googleweb.js
@@ -3,5 +3,5 @@ const navbox = document.querySelector('.navbox');
 
     menubar.addEventListener("click",()=>{
         console.log("button was clicked.");
-        navbox.classList.toggle('hide');
+        navbox.classList.toggle('active');
     })
